93162a7abca50fa08ad84f56de0d1d418740e28f
aurel <appiusa@student.ethz.ch>
2022-03-31 21:44:08 +0200

    added gripper sensor msg and changed grip cmd

---
+++ cpp_msgs/quadcopter_msgs/GripperSensor_msg.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace cpp_msg {
+
+struct GripperSensor_msg {
+
+  int force_front_left;
+  int force_front_right;
+  int force_back_left;
+  int force_back_right;
+};
+
+} // namespace cpp_msg
