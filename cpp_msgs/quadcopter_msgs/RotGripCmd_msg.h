92b2024577a0dfd4b60571f87b5dc1f7c255b780
aurel <appiusa@student.ethz.ch>
2022-04-05 10:31:26 +0200

    new msgs

--- cpp_msgs/quadcopter_msgs/RotGripCmd_msg.h
+++ cpp_msgs/quadcopter_msgs/RotGripCmd_msg.h
@@ -1,12 +1,15 @@
 #pragma once
 
-namespace cpp_msg {
+namespace cpp_msg
+{
 
-struct RotGripCmd_msg {
+  struct RotGripCmd_msg
+  {
 
-  int front_arm_deg;
-  int back_arm_deg;
-  // bool trigger_gripper;
-};
+    int front_arm_deg;
+    int back_arm_deg;
+    bool trigger_gripper;
+    bool get_sensor_val;
+  };
 
 } // namespace cpp_msg
